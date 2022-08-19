ff703318fd83de56f480e43b6a7fc8d107a2e3b9
ArjunNagar <arjunnagar579@gmail.com>
2022-08-19 10:02:48 +0530

    initial commit

---
+++ src/data.js
@@ -0,0 +1,68 @@
+export const projects = [
+  {
+    title: "Movie App",
+    subtitle: "React",
+    description:
+      "We all have our favourite movies and so this app let's user search movies and add their desired ones to favourites.",
+    image: require("./images/movieapp.jpg"),
+    link: "https://arjunnagar.github.io/basic-movie-app/",
+  },
+  {
+    title: "Search and Filter Countries",
+    subtitle: "React and Custom Pagination",
+    description:
+      "Combining the new found power of API's and my old interest in playing Atlas, this project displays countries filtered by its region. User can even search a country and I also learnt how to implement pagination.",
+    image: require("./images/country.jpg"),
+    link: "https://arjunnagar.github.io/my-countries-app/",
+  },
+  {
+    title: "Translator App",
+    subtitle: "React and Axios",
+    description:
+      "Since it's my first project working with an API; I made a translator app which supports 17 languages. The challenge was to detect source and target language code but sincere thanks to my old friend Google. I made it.",
+    image: require("./images/tanslator.png"),
+    link: "https://arjunnagar.github.io/basic-translator-app/",
+  },
+  {
+    title: "Notes App",
+    subtitle: "React Hooks and Local Storage",
+    description:
+      "In this project I learnt CRUD operations in React. User can add a note and upon return it will be there since the notes are saved in Local Storage.",
+    image: require("./images/notes.png"),
+    link: "https://arjunnagar.github.io/notes-app/",
+  },
+  {
+    title: "Survey App",
+    subtitle: "React",
+    description:
+      "Combining all the concepts I learnt so far, this project includes a registration form; a login form and finally a feedback form.",
+    image: require("./images/survey.jpg"),
+    link: "https://arjunnagar.github.io/feedback-registration-site/",
+  },
+  {
+    title: "Simple Cart App",
+    subtitle: "React",
+    description:
+      "Seeing my years of experience in web development I thought it would be appropriate to start by a basic cart app instead of building a whole e-commerce website.",
+    image: require("./images/cart.webp"),
+    link: "https://arjunnagar.github.io/simple-cart-app/",
+  },
+  {
+    title: "Quiz App",
+    subtitle: "React",
+    description:
+      "Although a simple project at first glance but it utilizes some major concepts like working with state objects, how to handle different state hooks, and render things based on state.",
+    image: require("./images/quiz.jpg"),
+    link: "https://arjunnagar.github.io/quiz-app/",
+  },
+  {
+    title: "Login Form",
+    subtitle: "Validation in React",
+    description:
+      "Starting with the basics, in this project I learnt how to handle user input and implemented conditional rendering.",
+    image: require("./images/login.webp"),
+    link: "https://arjunnagar.github.io/login-form/",
+  },
+];
+
+export const skills = ["HTML", "CSS", "JavaScript", "React"];
